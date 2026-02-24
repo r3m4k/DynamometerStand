628669292917b98313f9adc41ec585dda699b95d
r3m4k <romanovskiy_roma_2004@mail.ru>
2026-02-24 13:27:33 +0300

    Сделал фильтр Калмана шаблонным

--- Embedded/user/inc/Common/TriaxialData.hpp
+++ Embedded/user/inc/Common/TriaxialData.hpp
@@ -8,8 +8,8 @@
  *************************************************************************** */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef DATA_FRAME_HPP
-#define DATA_FRAME_HPP
+#ifndef TRIAXIAL_DATA_HPP
+#define TRIAXIAL_DATA_HPP
 
 /* Includes ------------------------------------------------------------------*/
 #include <cmath>
@@ -39,7 +39,7 @@ public:
      * @brief Конструктор по умолчанию, инициализирует координаты нулями 
      */
     TriaxialData(): x_coord(0), y_coord(0), z_coord(0) {}
-    
+
     /** 
      * @brief Конструктор с заданным общим значением для координат
      * @param val Общее значение координат
@@ -236,4 +236,4 @@ public:
     bool operator!=(const TriaxialData& other) const { return !(*this == other); }
 };
 
-#endif /*   DATA_FRAME_HPP   */
+#endif /*   TRIAXIAL_DATA_HPP   */
